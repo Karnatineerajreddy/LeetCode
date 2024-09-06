29f89be11119e0d8d8706242132dea3db73004bb
Karnatineerajreddy <101508032+Karnatineerajreddy@users.noreply.github.com>
2024-09-06 18:41:39 -0400

    Time: 20 ms (88.14%), Space: 16.3 MB (40.31%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,43 +2,39 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n=nums.size();
-        vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
 
-        for(int a=0;a<n;a++)
-        {
-            for(int b=a+1;b<n;b++)
-            {
-                int c=b+1;
-                int d=n-1;
-                while(c<d)
-                {
-                    long long sum=nums[a];
-                    sum+=nums[b];
-                    sum+=nums[c];
-                    sum+=nums[d];
-                    if(sum<target)
-                    {
-                        c++;
+        sort(nums.begin(),nums.end());
+        for(auto it:nums)
+        cout<<it <<" ";
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n-2;j++){
+                if(j >(i+1) && nums[j]==nums[j-1]) continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long long sum = nums[i];
+                    sum +=+ nums[j];
+                    sum +=nums[k];
+                    sum +=nums[l];
+                    if(sum>target) l--;
+                    else if(sum<target) k++;
+                    else{
+                        vector<int> temp={nums[i],nums[j],nums[k],nums[l]};
+                        ans.push_back(temp);
+
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
                     }
-                    else if(sum>target)
-                    {
-                        d--;
-                    }
-                    else
-                    {
-                        vector<int>v={nums[a],nums[b],nums[c],nums[d]};
-                        //if new quads then push to main vector
-                        if(find(ans.begin(),ans.end(),v)==ans.end()) 
-                        {
-                            ans.push_back(v);
-                        }
-                        c++;
-                        d--;
-                    }
+                    
+                    
                 }
             }
         }
         return ans;
+        
     }
 };
