cc072985e8b4c2f7f3113fcc5ab2b58d3c1577ae
Karnatineerajreddy <101508032+Karnatineerajreddy@users.noreply.github.com>
2024-09-06 18:42:13 -0400

    Time: 19 ms (90.03%), Space: 16.2 MB (40.31%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -5,11 +5,9 @@ public:
         vector<vector<int>> ans;
 
         sort(nums.begin(),nums.end());
-        for(auto it:nums)
-        cout<<it <<" ";
         for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<n-2;j++){
+            for(int j=i+1;j<n;j++){
                 if(j >(i+1) && nums[j]==nums[j-1]) continue;
                 int k=j+1;
                 int l=n-1;
