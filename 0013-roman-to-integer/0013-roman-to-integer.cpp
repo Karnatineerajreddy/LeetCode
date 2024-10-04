90c58ea56fe93498ab3a56c2771bdfdff3657916
Karnatineerajreddy <101508032+Karnatineerajreddy@users.noreply.github.com>
2024-10-04 17:32:44 -0400

    Time: 11 ms (55.64%), Space: 14.3 MB (24.18%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char,int> mpp={
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        int sum=0;
+        int n=s.size();
+        for (int i = 0; i < n; i++) {
+
+            if (i < n - 1 && mpp[s[i]] < mpp[s[i + 1]]) {
+                sum -= mpp[s[i]];
+            } else {
+                
+                sum += mpp[s[i]];
+            }
+        }
+
+        return sum;
+        
+    }
+};
