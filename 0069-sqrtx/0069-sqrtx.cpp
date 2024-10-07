96702f28f0c2b5601ae42d2861d6e446907c050e
Karnatineerajreddy <101508032+Karnatineerajreddy@users.noreply.github.com>
2024-10-07 14:00:46 -0400

    Time: 0 ms (100%), Space: 8.1 MB (87.53%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0) return 0;  // Handle edge case where x is 0
+        if(x == 0) return 0; 
         int low = 1, high = x;
-        while (low <= high) {
-            long long mid = low + (high - low) / 2;  // To avoid overflow in calculating mid
-            if (mid * mid == x) {
-                return mid;  // Exact square root found
-            } else if (mid * mid < x) {
+        while(low <= high) {
+            long long mid = low + (high - low) / 2;
+            long long val=mid*mid;
+            if(val<= x) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
-        return high;  // The integer square root
+        return high;
     }
-};
+};  
