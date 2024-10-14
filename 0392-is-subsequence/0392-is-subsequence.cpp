7a08e0be453a5cbf5d82ca9ccd400777d727f85f
Karnatineerajreddy <101508032+Karnatineerajreddy@users.noreply.github.com>
2024-10-14 18:14:46 -0400

    Time: 3 ms (45.45%), Space: 7.8 MB (69.05%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int n1 = s.size();
-        int n2 = t.size();
-        
-        int i = 0; // Pointer for string s
-        int j = 0; // Pointer for string t
-        
-        // Traverse both strings
-        while (i < n1 && j < n2) {
-            if (s[i] == t[j]) {
-                i++; // Move pointer for s if there's a match
+        int i=0,j=0;
+        int n1=s.size(),n2=t.size();
+        while(i<n1 && j<n2 ){
+            if(s[i]==t[j]){
+                i++;
             }
-            j++; // Always move pointer for t
+            j++;
         }
+        return i==n1;
+
         
-        // If we have traversed all of s, it's a subsequence
-        return i == n1;
     }
 };
