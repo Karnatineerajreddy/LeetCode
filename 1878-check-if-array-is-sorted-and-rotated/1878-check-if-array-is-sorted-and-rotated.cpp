7af823109fde13a006c07c37f943503acea176f9
Karnatineerajreddy <101508032+Karnatineerajreddy@users.noreply.github.com>
2024-10-15 21:21:48 -0400

    Time: 2 ms (44.67%), Space: 10.5 MB (58.64%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -5,8 +5,9 @@ public:
         int count=0;
         for(int i=0;i<n;i++){
             if(nums[i]>nums[(i+1)%n]) count++;
+            if(count>1) return false;
         }
-        if(count>1) return false;
+        
         return true;
     }
 };
