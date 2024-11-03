1f288454f205136443319086cecb24b39179ac57
Karnatineerajreddy <101508032+Karnatineerajreddy@users.noreply.github.com>
2024-11-03 12:04:13 -0500

    Time: 0 ms (100%), Space: 14.6 MB (91.3%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -16,9 +16,7 @@ public:
         for(int i=0;i<n;i++){
             fast=fast->next;
         }
-        if(fast==NULL){
-            return head->next;
-        }
+        if(fast==NULL) return head->next;
         while(fast->next!=NULL){
             slow=slow->next;
             fast=fast->next;
