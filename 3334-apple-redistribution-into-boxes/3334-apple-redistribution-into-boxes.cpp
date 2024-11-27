9aa626efbab04a161479b798c90fc1304d23faae
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2024-11-27 11:28:09 -0500

    Time: 0 ms (100%), Space: 33.1 MB (20.62%) - LeetHub

--- 3334-apple-redistribution-into-boxes/3334-apple-redistribution-into-boxes.cpp
+++ 3334-apple-redistribution-into-boxes/3334-apple-redistribution-into-boxes.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
+    int sumofapples(vector<int>&apple){
+        int sum=0;
+        for(int i=0;i<apple.size();i++){
+            sum=sum+apple[i];
+        }
+        return sum;
+    }
     int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
-        int sum = accumulate(apple.begin(), apple.end(), 0);
-        if (capacity.empty()) return -1; // No boxes available
+        int sum=sumofapples(apple);
+        if (capacity.empty()) return -1;
         if (apple.empty()) return 0;
         sort(capacity.begin(), capacity.end(), greater<int>());
-        int count = 0;
-
-        for (int i = 0; i < capacity.size(); i++) {
-            sum -= capacity[i];
+        int final,count=0;
+        for(int i=0;i<capacity.size();i++){
+            sum=sum-capacity[i];
             count++;
-            if (sum <= 0) return count;
-        }
+            if(sum<=0){
+                return count;
+            }
 
+        }
         return -1;
+        
     }
 };
