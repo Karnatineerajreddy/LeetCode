76c222793fda8a6260f566a019d9dca5f009661a
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2024-12-24 15:09:39 -0500

    Time: 0 ms (100%), Space: 7.8 MB (72.85%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        if(n==1) return true;
-        if(n==4) return false;
+        if(n==1 || n==7) return true;
+        if(n<10) return false;
         int sum=0;
         while(n!=0){
             int n1=n%10;
