bc8f875d2c7fcbbc0115d01138817bab070eb9b1
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2024-12-24 15:06:24 -0500

    Time: 0 ms (100%), Space: 7.8 MB (78.44%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -4,7 +4,7 @@ public:
         if(n==1) return true;
         if(n==4) return false;
         int sum=0;
-        while(n>0){
+        while(n!=0){
             int n1=n%10;
             sum=n1*n1+sum;
             n=n/10;
