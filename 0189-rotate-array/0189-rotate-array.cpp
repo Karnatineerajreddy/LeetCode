429e39c830c1eed793524e1239372e2258f2127d
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2024-12-27 13:54:03 -0500

    Time: 3 ms (9.57%), Space: 30 MB (18.09%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        if (n == 0) return;
-        int d = k % n;
-        if (d == 0) return;
-        std::vector<int> temp(d);
+        int n=nums.size();
 
-        for (int i = 0; i < d; i++) {
-            temp[i] = nums[n - d + i];
-        }
+        int d=k%n;
+        if(n==0) return;
+        if(d==0) return;
+        vector<int> temp(nums.end() - d, nums.end());
         for (int i = n - 1; i >= d; i--) {
             nums[i] = nums[i - d];;
         }
+        
         for(int i=0;i<d;i++){
             nums[i]=temp[i];
         }
+
         
     }
 };
