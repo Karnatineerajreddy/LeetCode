6acb15bcbe0d9062eb32cbf6efff0c22e51f52cc
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-01-07 14:31:10 -0500

    Time: 0 ms (100%), Space: 8.7 MB (12.39%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
-    double power(double x, long long n) {
-        double result = 1.0;
-        while (n > 0) {
-            if (n % 2 == 1) { 
-                result *= x;
+    double myPow(double x, int n) {
+        double ans =1;
+        long long nn=n;
+        if(nn<0) nn=-nn;
+        while(nn>0){
+            if(nn%2==0){
+                x=x*x;
+                nn=nn/2;
             }
-            x *= x;   
-            n /= 2;   
-        }
-        return result;
-    }
-    double myPow(double x, int u) {
-        long long n=u;
-        double sum;
-        if(x==0) return double(0);
-        else if(x==1) return double(1);
-        if(n>0){
-            sum=power(x,n);
-        }
-        else{
-            double temp=1/x;
-            n=-n;
-            sum=power(temp,n1);
-        }
+            else{
+                ans=ans*x;
+                nn=nn-1;
+            }
 
-
-        return sum;
+        }
+        if(n<0) ans=double(1.0)/double(ans);
+        return ans;
     }
 };
