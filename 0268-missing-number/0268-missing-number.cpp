17e44f7bb521113d6056e44264ab2e929c197e60
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-01-11 16:46:23 -0500

    Time: 0 ms (100%), Space: 21.8 MB (31.67%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,12 +3,10 @@ public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
         int sum=n*(n+1)/2;
-        int s=0;
+        int sum2=0;
         for(int i=0;i<n;i++){
-            s+=nums[i];
+            sum2+=nums[i];
         }
-        return (sum-s);
-
-        
+        return sum-sum2;
     }
 };
