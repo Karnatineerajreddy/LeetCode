b3fbb0ea6653195ef273f530d9e38e5e41c6a2fc
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-01-14 14:26:50 -0500

    Time: 0 ms (100%), Space: 8.1 MB (82.51%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,8 +3,8 @@ public:
     int hammingWeight(int n) {
         int count=0;
         while(n>0){
-            count+=n%2;
-            n=n/2;
+            count+=n & 1;
+            n=n>>1;
         }
         return count;
     }
