ff2bc0410070f7a70d61c4dc37553312860b3ea9
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-01-14 14:15:45 -0500

    Time: 0 ms (100%), Space: 8 MB (17.06%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int sum=0;
-        while(n!=0){
-            sum +=n%2;
-            n=n/2;
-        }
-        if(sum==1) return true;
-        else return false;
+        return (n > 0) && ((n & (n - 1)) == 0);
     }
 };
