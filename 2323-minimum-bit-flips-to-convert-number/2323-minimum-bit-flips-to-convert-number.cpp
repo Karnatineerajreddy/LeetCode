074c9b33e3b0206a0fa740c4c33afcf2d5cebdd1
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-01-16 16:00:41 -0500

    Time: 2 ms (2.69%), Space: 7.9 MB (53.85%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -3,7 +3,7 @@ public:
     int minBitFlips(int start, int goal) {
         int ans=start^goal;
         int count=0;
-        for(int i=0;i<31;i++){
+        for(int i=0;i<=31;i++){
             if(ans & (1<<i)) count++;
         }
         return count;
