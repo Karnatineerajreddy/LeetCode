97a4546b4c5e274a9a34fd30fd29d4a453fcd76a
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-01-22 12:52:42 -0500

    Time: 0 ms (100%), Space: 14 MB (37.13%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        long long xorr=0;
+        vector<int> ans;
+        long long b1=0,b2=0;
+        for(int i=0;i<nums.size();i++) xorr ^=nums[i];
+        int rightset=(xorr & xorr-1) ^ xorr;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]&rightset) b1^=nums[i];
+            else b2^=nums[i];
+        }
+        ans.push_back(b1);
+        ans.push_back(b2);
+        return ans;
+
+    }
+};
