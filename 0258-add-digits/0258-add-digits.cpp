8abf980dd5d4b926907abfcd284e44aad947018d
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-01-26 16:18:46 -0500

    Time: 0 ms (100%), Space: 8.7 MB (11.67%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int addDigits(int num) {
-        if (num < 10) return num;
-
-        int add = 0;
-
-        while (num != 0) {
-            add += num % 10; 
-            num /= 10;    
-        }
-
-
-        return addDigits(add);
+        if(num==0) return 0;
+        if(num%9==0) return 9;
+        else return num%9;
     }
 };
