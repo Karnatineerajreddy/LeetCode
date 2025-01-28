94100090ef01c58d692e5718f6c1d43aaa90b446
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-01-27 22:05:07 -0500

    Time: 2 ms (9.07%), Space: 22.4 MB (58.98%) - LeetHub

--- 3704-count-partitions-with-even-sum-difference/3704-count-partitions-with-even-sum-difference.cpp
+++ 3704-count-partitions-with-even-sum-difference/3704-count-partitions-with-even-sum-difference.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
-    int findsum(vector<int> nums) {
-        int sum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-        }
-        return sum;
-    }
+
 
     int countPartitions(vector<int>& nums) {
         int count = 0;
-        int totalSum = findsum(nums);  
+        int totalSum = accumulate(nums.begin(),nums.end(),0);  
 
         int first = 0;  
         for (int i = 0; i < nums.size() - 1; i++) {
