618e8c30b57e9868823d571030a0618e1847f8e3
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-01-27 23:40:26 -0500

    Time: 0 ms (100%), Space: 7.9 MB (23.39%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.cpp
+++ 2083-three-divisors/2083-three-divisors.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     bool isThree(int n) {
-        int count =0;
+        int count=0;
         for(int i=1;i<=n;i++){
-            if(n%i==0) count++;
-            
+            if(n%i==0) count++; 
+            if (count>3) return false;
         }
-        if(count==3){
-            return true;
-            }
-        else return false;
-        
+        return (count==3);
     }
 };
