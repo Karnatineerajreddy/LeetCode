9409a4c94efa2fe6107bec53549b3fc9c723367a
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-02-05 19:50:14 -0500

    Time: 0 ms (100%), Space: 9.2 MB (53.13%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char xxor=0;
-        for(auto it:s) xxor^=it;
-        for(auto it:t) xxor^=it;
-        return xxor;
+        int ans1=0,ans2=0;
+        for(auto it:s) ans1+=it;
+        for(auto it:t) ans2+=it;
+        return (ans2-ans1);
     }
 };
