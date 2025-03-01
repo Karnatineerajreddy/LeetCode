e14e409c5f7acc1bba65ea1382f9338e09273d6d
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-03-01 13:26:48 -0500

    Time: 0 ms (100%), Space: 27.3 MB (20.61%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> preffix(vector<int> &nums){
+            vector<int> pmax(nums.size());;
+            pmax[0]=nums[0];
+            for(int i=1;i<nums.size();i++){
+                pmax[i]=max(pmax[i-1],nums[i]);
+            }
+            return pmax;
+    }
+    vector<int> suffix(vector<int> &nums){
+            vector<int> pmax(nums.size());;
+            pmax[nums.size()-1]=nums[nums.size()-1];
+            for(int i=nums.size()-2;i>=0;i--){
+                pmax[i]=max(pmax[i+1],nums[i]);
+            }
+            return pmax;
+    }
+    
+
+
+    int trap(vector<int>& height) {
+        if (height.empty()) return 0;
+        int total=0;
+        vector<int> pmax,smax;
+        pmax=preffix(height);
+        smax=suffix(height);
+        for(int i=0;i<height.size();i++){
+            int leftmax=pmax[i];
+            int rightmax=smax[i];
+            if(height[i]<leftmax && height[i]< rightmax){
+                total+=min(leftmax,rightmax)-height[i];
+            }
+        }
+        return total;
+    }
+
+};
