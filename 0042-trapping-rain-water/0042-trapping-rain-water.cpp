944cc7c104f1c4447082ca662bcec747d5b2d256
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-03-01 13:59:28 -0500

    Time: 0 ms (100%), Space: 25.9 MB (80.63%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
-    vector<int> preffix(vector<int> &nums){
-            vector<int> pmax(nums.size());;
-            pmax[0]=nums[0];
-            for(int i=1;i<nums.size();i++){
-                pmax[i]=max(pmax[i-1],nums[i]);
-            }
-            return pmax;
-    }
-    vector<int> suffix(vector<int> &nums){
-            vector<int> smax(nums.size());;
-            smax[nums.size()-1]=nums[nums.size()-1];
-            for(int i=nums.size()-2;i>=0;i--){
-                smax[i]=max(smax[i+1],nums[i]);
-            }
-            return smax;
-    }
-    
-
-
-    int trap(vector<int>& height) {
-        if (height.empty()) return 0;
+    int trap(vector<int>& arr) {
+        int lmax=0;
+        int rmax=0;
         int total=0;
-        vector<int> pmax,smax;
-        pmax=preffix(height);
-        smax=suffix(height);
-        for(int i=0;i<height.size();i++){
-            int leftmax=pmax[i];
-            int rightmax=smax[i];
-            if(height[i]<leftmax && height[i]< rightmax){
-                total+=min(leftmax,rightmax)-height[i];
+        int l=0,r=arr.size()-1;
+        while(l<r){
+            if(arr[l]<=arr[r]){
+                if(lmax>arr[l]) total+=lmax-arr[l];
+                else lmax=arr[l];
+                l++;
+            }
+            else{
+                if(rmax>arr[r]) total+=rmax-arr[r];
+                else rmax=arr[r];
+                r--;
             }
         }
         return total;
     }
-
 };
