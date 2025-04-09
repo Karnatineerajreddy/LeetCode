7d6c74c18f36db534b31925b5e800ad50f49ece7
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-04-09 17:32:25 -0400

    Time: 8 ms (36%), Space: 11.6 MB (71.57%) - LeetHub

--- 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.cpp
+++ 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.cpp
@@ -3,8 +3,8 @@ public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
         int n=nums.size();
         int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
                 for(int k=j+1;k<n;k++){
                     if(nums[j]-nums[i]==diff && nums[k]-nums[j]==diff) count++;
 
@@ -12,5 +12,6 @@ public:
             }
         }
         return count;
+        //o(n^3)
     }
 };
