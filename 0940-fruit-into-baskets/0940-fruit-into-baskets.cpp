c468f6326bc6b06c6f77a29cbe1959e30969392c
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-04-11 14:52:13 -0400

    Time: 34 ms (78.02%), Space: 75 MB (76.4%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -8,12 +8,9 @@ public:
         while(r<fruits.size()){
             mpp[fruits[r]]++;
             if(mpp.size()>2){
-                while(mpp.size() > 2){
-                    mpp[fruits[l]]--;
-                    if(mpp[fruits[l]]==0) mpp.erase(fruits[l]);
-                    l++;
-                }
-
+                mpp[fruits[l]]--;
+                if(mpp[fruits[l]]==0) mpp.erase(fruits[l]);
+                l++;
             }
             if(mpp.size()<=2) maxi=max(maxi,r-l+1);
             r++;
