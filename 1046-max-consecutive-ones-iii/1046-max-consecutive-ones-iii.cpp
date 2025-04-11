c96f547bec595a170abfd98452b8921658c87753
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-04-11 12:20:50 -0400

    Time: 0 ms (100%), Space: 59.2 MB (91.48%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -7,8 +7,10 @@ public:
         int zeros=0;
         while(r<nums.size()){
             if(nums[r]==0) zeros++;
-            while(zeros>k){
-                if(nums[l]==0) zeros--;
+            if(zeros>k){
+                if(nums[l]==0){
+                    zeros--;
+                }
                 l++;
             }
             if(zeros<=k) maxi=max(maxi,r-l+1);
