cb9d9ad45a7830df8981fb14c85adf2317f46fa3
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-04-13 14:19:03 -0400

    Time: 7 ms (51.26%), Space: 11.1 MB (21.11%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0, maxCount = 0, res = 0;
+        unordered_map<char, int> mpp;
+
+        for (int r = 0; r < s.size(); ++r) {
+            mpp[s[r]]++;
+            maxCount = max(maxCount, mpp[s[r]]);
+            while ((r - l + 1) - maxCount > k) {
+                mpp[s[l]]--;
+                l++;
+            }
+
+            res = max(res, r - l + 1);
+        }
+
+        return res;
+    }
+};
