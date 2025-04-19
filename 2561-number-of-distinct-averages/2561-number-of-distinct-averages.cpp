065634bfc6a7c66af882d72f7a6e7a376ba3bdce
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-04-19 15:44:47 -0400

    Time: 0 ms (100%), Space: 9.4 MB (81.84%) - LeetHub

--- 2561-number-of-distinct-averages/2561-number-of-distinct-averages.cpp
+++ 2561-number-of-distinct-averages/2561-number-of-distinct-averages.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int distinctAverages(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(),nums.end());
         set<double> st;
-        int i = 0, j = nums.size() - 1;
-        while (i < j) {
-            double avg = (nums[i] + nums[j]) / 2.0;
+        int i=0,j=nums.size()-1;
+        while(i<j){
+            double avg=(nums[i]+nums[j])/2.0;
             st.insert(avg);
             i++;
             j--;
