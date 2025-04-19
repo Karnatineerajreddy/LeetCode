4f244623e3468631ce472801104db775af1c58b6
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-04-19 17:15:10 -0400

    Time: 0 ms (100%), Space: 10 MB (62.48%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        int sub = 1 << nums.size();
-        vector<vector<int>> ans;
-
-        for (int i = 0; i < sub; i++) { 
-            vector<int> subset;
-            for (int j = 0; j < nums.size(); j++) { 
-                if (i & (1 << j)) { 
-                    subset.push_back(nums[j]);
-                }
-            }
-            ans.push_back(subset); 
+    void Find_sub(int i,vector<int>& nums,vector<vector<int>>& ans,vector<int> &current) {
+        if(i>=nums.size()){
+            ans.push_back(current);
+            return;
         }
-        
+        current.push_back(nums[i]);
+        Find_sub(i+1,nums,ans,current);
+        current.pop_back();
+        Find_sub(i+1,nums,ans,current);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> current;
+        Find_sub(0,nums,ans,current);
         return ans;
     }
 };
