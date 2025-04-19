5ceef3f4537c805ad75f8fcf65ad5af5673e86e4
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-04-19 14:20:14 -0400

    Time: 3 ms (22.86%), Space: 13.2 MB (16.94%) - LeetHub

--- 1210-mean-of-array-after-removing-some-elements/1210-mean-of-array-after-removing-some-elements.cpp
+++ 1210-mean-of-array-after-removing-some-elements/1210-mean-of-array-after-removing-some-elements.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     double trimMean(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int remove_ele=(nums.size()*0.05);
-        double sum=accumulate(nums.begin()+remove_ele,nums.end()-remove_ele,0);
+        sort(nums.begin(), nums.end());
+        int remove_ele = nums.size() * 0.05;
+        double sum = 0;
+
+        for (int i = remove_ele; i < nums.size() - remove_ele; i++) {
+            sum += nums[i];
+        }
+
         return sum / (nums.size() - 2 * remove_ele);
-        
     }
 };
