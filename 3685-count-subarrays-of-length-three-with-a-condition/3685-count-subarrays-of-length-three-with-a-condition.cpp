4f862363f4f14c325752bf62dcfc15a22c6c7ffd
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-04-26 23:16:18 -0400

    Time: 0 ms (100%), Space: 48.5 MB (11.51%) - LeetHub

--- 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cpp
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cpp
@@ -5,7 +5,7 @@ public:
         if (nums.size() < 3) return 0; 
         
         while (j < nums.size()) {
-            if (nums[i+1] % 2 == 0 && nums[i] + nums[j] == nums[i+1] / 2) {
+            if (nums[i+1] % 2 == 0 && nums[i] + nums[j] == nums[j-1] / 2) {
                 count++;
             }
             i++;
