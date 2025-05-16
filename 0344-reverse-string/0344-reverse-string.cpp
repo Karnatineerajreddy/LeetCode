1d04c69839db458cf9aaba61e236aad1a463b8b7
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-05-15 23:54:03 -0400

    Time: 0 ms (100%), Space: 27.2 MB (52.13%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    void find_reverse(int i,vector<char> &s,int n){
-        if(i>=n/2) return;
-        swap(s[i],s[n-i-1]);
-        find_reverse(i+1,s,n);
-    }
     void reverseString(vector<char>& s) {
-        return find_reverse(0,s,s.size());
+        int i=0,j=s.size()-1;
+        while(i<j){
+            swap(s[i],s[j]);
+            i++;
+            j--;
+        }
     }
 };
