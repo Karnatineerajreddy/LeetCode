a33ece0b16f14b10492bcaf5e3d1ff2a13246656
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-05-24 15:20:48 -0400

    Time: 4 ms (15.6%), Space: 33.7 MB (12.1%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.cpp
+++ 3194-find-words-containing-character/3194-find-words-containing-character.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
+    bool findind(string word,char x){
+        int i=0,found=0;
+        while(i<word.size()){
+            if(word[i]!=x) i++;
+            else{
+                found=1;
+                break;
+            } 
+        }
+        if(found==1) return true;
+        else return false;
+    }
     vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int> ans;
         for(int i=0;i<words.size();i++){
-            if(words[i].find(x)!=string::npos) ans.push_back(i);
+            if(findind(words[i], x)) {
+                ans.push_back(i);
+            }
         }
         return ans;
     }
