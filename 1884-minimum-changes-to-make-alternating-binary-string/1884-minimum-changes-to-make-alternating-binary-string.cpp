2c65a16e508b9ae9e68a70d722ff14071bba4130
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-06-21 17:13:53 +0530

    Time: 0 ms (100%), Space: 9.3 MB (78.61%) - LeetHub

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int count1=0,count2=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=(i%2==0?'0':'1')) count1++;
+            if(s[i]!=(i%2==0?'1':'0')) count2++;
+        }
+        return min(count1,count2);
+    }
+};
