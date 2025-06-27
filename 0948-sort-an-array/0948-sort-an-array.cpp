3b5be638ecaa72e744cfccd49e63e931f6e1c612
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-06-27 17:47:59 +0530

    Time: 620 ms (30.61%), Space: 295.7 MB (17.21%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,16 +1,40 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        int mini = *min_element(nums.begin(), nums.end());
-        int maxi = *max_element(nums.begin(), nums.end());
-        int range = maxi - mini + 1;
-        vector<int> count(range, 0);
-        vector<int> output(nums.size());
-        for (int num : nums) count[num - mini]++;
-        for (int i = 1; i < range; i++) count[i] += count[i - 1];
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            output[--count[nums[i] - mini]] = nums[i];
+    void merge(vector<int> &nums,int low,int mid,int high){
+        vector<int> temp;
+        int left=low;
+        int right=mid+1;
+        while(left<=mid && right<=high){
+            if(nums[left]<=nums[right]){
+                temp.push_back(nums[left]);
+                left++;
+            }
+            else{
+                temp.push_back(nums[right]);
+                right++;
+            }
         }
-        return output;
+        while(left<=mid){
+            temp.push_back(nums[left]);
+            left++;
+        }
+        while(right<=high){
+            temp.push_back(nums[right]);
+            right++;
+        }
+        for(int i=low;i<=high;i++){
+            nums[i]=temp[i-low];
+        }
+    }
+    void ms(vector<int> &nums,int low,int high){
+        if(low>=high) return;
+        int mid=(low+high)/2;
+        ms(nums,low,mid);
+        ms(nums,mid+1,high);
+        merge(nums,low,mid,high);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        ms(nums,0,nums.size()-1);
+        return nums;
     }
 };
