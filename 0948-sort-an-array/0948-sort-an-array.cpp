36d2934e6b9826b4a14262a3c5ca78349884c14a
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-06-28 14:17:20 +0530

    Time: 2772 ms (5.02%), Space: 73.9 MB (60.31%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
-    void merge(vector<int> &nums,int low,int mid,int high){
-        vector<int> temp;
-        int left=low;
-        int right=mid+1;
-        while(left<=mid && right<=high){
-            if(nums[left]<=nums[right]){
-                temp.push_back(nums[left]);
-                left++;
-            }
-            else{
-                temp.push_back(nums[right]);
-                right++;
-            }
+    int partition(vector<int> &nums,int low,int high){
+        int pivot=nums[low];
+        int i=low,j=high;
+        while(i<j){
+            while (i <= high-1 && nums[i] <= pivot) i++;  
+            while (j >= low+1 && nums[j] > pivot) j--;
+            if(i<j) swap(nums[i],nums[j]);
         }
-        while(left<=mid){
-            temp.push_back(nums[left]);
-            left++;
-        }
-        while(right<=high){
-            temp.push_back(nums[right]);
-            right++;
-        }
-        for(int i=low;i<=high;i++){
-            nums[i]=temp[i-low];
-        }
+        swap(nums[low],nums[j]);
+        return j;
     }
-    void ms(vector<int> &nums,int low,int high){
-        if(low>=high) return;
-        int mid=(low+high)/2;
-        ms(nums,low,mid);
-        ms(nums,mid+1,high);
-        merge(nums,low,mid,high);
+    void qs(vector<int> &nums,int low,int high){
+        if(low<high){
+            int pindex=partition(nums,low,high);
+            qs(nums,low,pindex-1);
+            qs(nums,pindex+1,high);
+
+        }
     }
     vector<int> sortArray(vector<int>& nums) {
-        ms(nums,0,nums.size()-1);
+        qs(nums,0,nums.size()-1);
         return nums;
     }
 };
