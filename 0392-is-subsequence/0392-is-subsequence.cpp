042e68ce4955fec3871e293636e9ad1e7f60496d
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-06-30 23:06:52 +0530

    Time: 0 ms (100%), Space: 8.5 MB (66.67%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int i=0,j=0;
-        int n1=s.size(),n2=t.size();
-        while(i<n1 && j<n2 ){
-            if(s[i]==t[j]){
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j]) {
                 i++;
             }
             j++;
         }
-        return i==n1;
-
-        
+        return i==s.size();
+            
     }
 };
