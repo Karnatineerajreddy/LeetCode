dbad58860b935ac979855caec3a92585e2cfde06
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-07-09 17:44:54 +0530

    Time: 0 ms (100%), Space: 14.1 MB (60.91%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void Find_comb(int ind,int t,vector<int> &nums,vector<int> ds,vector<vector<int>> &ans){
-        if(t==0){
+    void find_combinations(int ind,int target, vector<int>& nums,vector<int> &ds,vector<vector<int>> &ans){
+        if(target==0){
             ans.push_back(ds);
             return;
         }
         for(int i=ind;i<nums.size();i++){
             if(i>ind && nums[i]==nums[i-1]) continue;
-            if(nums[i]>t) break;
+            if(nums[i]>target) break;
             ds.push_back(nums[i]);
-            Find_comb(i+1,t-nums[i],nums,ds,ans);
+            find_combinations(i+1,target-nums[i],nums,ds,ans);
             ds.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<int> ds;
+        sort(candidates.begin(),candidates.end());
         vector<vector<int>> ans;
-        sort(candidates.begin(), candidates.end());
-        Find_comb(0,target,candidates,ds,ans);
+        vector<int> ds;
+        find_combinations(0,target,candidates,ds,ans);
         return ans;
     }
 };
