ac1c8e5324167658653488ff045dc6b1e65bfb1b
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-07-15 18:19:16 +0530

    Time: 4 ms (47.28%), Space: 9 MB (14.85%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
+    bool power_three(int n){
+        if(n<=0) return false;
+        if(n==1) return true;
+        if(n%3!=0) return false;
+        return power_three(n/3);
+    }
     bool isPowerOfThree(int n) {
-        if (n<=0) return false;
-        else if(n==3 || n==1) return true;
-        else if(n%3==0) return isPowerOfThree(n/3);
-        return false;
-
-        
+        return power_three(n);
     }
 };
