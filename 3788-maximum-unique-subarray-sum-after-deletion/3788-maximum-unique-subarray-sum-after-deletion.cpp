c85a825cb73f4f952855c01f630af7296cf0754a
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-07-25 20:13:59 +0530

    Time: 0 ms (100%), Space: 29.3 MB (86.99%) - LeetHub

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        set<int> st;
+        int maxnegative=INT_MIN,ans=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0) st.insert(nums[i]);
+            else{
+                maxnegative=max(maxnegative,nums[i]);
+            }
+        }
+        for(auto it:st){
+            ans+=it;
+        }
+        return ans==0 ? maxnegative : ans;
+    }
+};
