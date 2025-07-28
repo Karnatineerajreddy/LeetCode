2fa083edee521ea84e4d7bdcdd64b617d10be8cc
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-07-28 16:11:35 +0530

    Time: 0 ms (100%), Space: 26.1 MB (67.21%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         if(nums.size()==1) return nums[0];
-        if(nums[0]!=nums[1]) return nums[0];
-        if(nums[nums.size()-1]!=nums[nums.size()-2]) return nums[nums.size()-1];
-        int l=1,r=nums.size()-2;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
-            else if(mid%2==1 && nums[mid]==nums[mid-1] || mid%2==0 && nums[mid]==nums[mid+1])           l=mid+1;
-            else r=mid-1;
+        for(int i=0;i<nums.size();i++){
+            if(i==0){
+                if(nums[i]!=nums[i+1]) return nums[0];
+                continue;
+            }
+            if(i==nums.size()-1){
+                if(nums[i]!=nums[i-1]) return nums[nums.size()-1];
+                continue;
+            }
+            if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]) return nums[i];
         }
         return -1;
-
     }
 };
