bfa53baabc39ab878e0e1c08ef80c64fc1a3726a
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-07-29 14:35:02 +0530

    Time: 0 ms (100%), Space: 7.7 MB (64.11%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        vector<int> dp(n+1);
         if(n==0) return 0;
         if(n==1) return 1;
-        dp[0]=0,dp[1]=1;
+        int prev1=1,prev2=0;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int curri=prev1+prev2;
+            prev2=prev1;
+            prev1=curri;
         }
-        return dp[n];
+        return prev1;
     }
 };
