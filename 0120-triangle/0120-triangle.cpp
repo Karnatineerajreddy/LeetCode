ceba4834c81e58f28f1d1a9a65a40dc29306b7c6
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-08-31 17:08:57 -0400

    Time: 0 ms (100%), Space: 12.9 MB (70.2%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,17 +3,19 @@ public:
 
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
-        vector<vector<int>> dp(m,vector<int>(m,0));
+        vector<int> dp(m);
         for(int j=0;j<m;j++){
-            dp[m-1][j]=triangle[m-1][j];
+            dp[j]=triangle[m-1][j];
         }
         for(int i=m-2;i>=0;i--){
+            vector<int> curr(m);
             for(int j=i;j>=0;j--){
-                int down=triangle[i][j]+dp[i+1][j];
-                int downDia=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(down,downDia);
+                int down=triangle[i][j]+dp[j];
+                int downDia=triangle[i][j]+dp[j+1];
+                curr[j]=min(down,downDia);
             }
+            dp=curr;
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
