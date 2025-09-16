fa1b54bd09ae13ed2f117801efde004d13c9ea80
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-09-17 02:23:35 +0530

    Time: 0 ms (100%), Space: 36.6 MB (95.47%) - LeetHub

--- 4012-earliest-time-to-finish-one-task/4012-earliest-time-to-finish-one-task.cpp
+++ 4012-earliest-time-to-finish-one-task/4012-earliest-time-to-finish-one-task.cpp
@@ -3,9 +3,10 @@ public:
     int earliestTime(vector<vector<int>>& tasks) {
         int mini=INT_MAX;
         for(int i=0;i<tasks.size();i++){
-            int sum=0;
-            for(int j=0;j<=1;j++){
+            int sum=0,j=0;
+            while(j<=1){
                 sum+=tasks[i][j];
+                j++;
             }
             mini=min(mini,sum);
         }
