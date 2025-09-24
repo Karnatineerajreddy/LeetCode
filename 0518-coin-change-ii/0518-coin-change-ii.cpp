4ed54ae3949752975904a208ff8995f1050b40a2
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-09-25 00:15:40 +0530

    Time: 31 ms (28.62%), Space: 36.4 MB (20.82%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int f(int ind,int t,vector<int> & coins,vector<vector<int>> &dp){
-        if(ind==0){
-            return (t%coins[ind]==0);
-        }
-        if(dp[ind][t]!=-1) return dp[ind][t];
-        long notpick=f(ind-1,t,coins,dp);
-        long pick=0;
-        if(coins[ind]<=t) pick=f(ind,t-coins[ind],coins,dp);
-        return dp[ind][t]=pick+notpick;
-    }
+
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(),vector<int> (amount+1,-1));
-        return f(coins.size()-1,amount,coins,dp);
+        vector<vector<int>> dp(coins.size(),vector<int> (amount+1,0));
+        for(int t=0;t<=amount;t++){
+            dp[0][t]=(t%coins[0]==0);
+        }
+        for(int i=1;i<coins.size();i++){
+            for(int j=0;j<=amount;j++){
+                long notpick=dp[i-1][j];
+                long pick=0;
+                if(coins[i]<=j) pick=dp[i][j-coins[i]];
+                dp[i][j]=pick+notpick;
+            }
+        }
+        return dp[coins.size()-1][amount];
     }   
 };
