8b59cd705c2856f0733ae26946b19d3008172b60
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-10-08 03:54:38 +0530

    Time: 0 ms (100%), Space: 24.2 MB (41.23%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -4,9 +4,10 @@ public:
         int size=flowerbed.size(),count=0;
         for(int i=0;i<size;i++){
             if(flowerbed[i]==0){
-                int leftEmpty=(i==0) || (flowerbed[i-1]==0);
-                int rightEmpty=(i==size-1) || (flowerbed[i+1]==0);
-                if(leftEmpty && rightEmpty){
+                int left=1,right=1;
+                if(i==0 || flowerbed[i-1]==0) left=0;
+                if(i==size-1 || flowerbed[i+1]==0) right=0;
+                if(left==0 && right==0){
                     flowerbed[i]=1;
                     count++;
                     if(count>=n) return true;
