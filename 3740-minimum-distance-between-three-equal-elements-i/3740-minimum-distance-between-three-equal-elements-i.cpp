9cc04eb65e1ff9df78a3a2ecb1c3aa90566d01bb
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-11-22 20:36:30 -0500

    Time: 6 ms (36.26%), Space: 29.8 MB (31.98%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> findTriple(vector<int> &nums){
-        unordered_map<int, vector<int>> mpp;
+        unordered_map<int,int> mpp;
         vector<int> ans;
 
-        for (int i = 0; i < nums.size(); i++) {
-            mpp[nums[i]].push_back(i);
+        for (auto it : nums) {
+            mpp[it]++;
         }
 
-        for (auto &it : mpp) {
-            if (it.second.size() >= 3) {
+        for (auto it : mpp) {
+            if (it.second >= 3) {
                 ans.push_back(it.first);
             }
         }
@@ -18,11 +18,9 @@ public:
     }
 
     int minimumDistance(vector<int>& nums) {
-        // find all values that appear at least 3 times
         vector<int> num = findTriple(nums);
         if (num.size() == 0) return -1;
 
-        // map each number to all its positions
         unordered_map<int, vector<int>> pos;
         for (int i = 0; i < nums.size(); i++) {
             pos[nums[i]].push_back(i);
@@ -30,24 +28,17 @@ public:
 
         int mini = INT_MAX;
 
-        // For each number, check all triples of positions
+       
         for (int val : num) {
-            auto &indexes = pos[val];
+            vector<int>& idx = pos[val];
 
-            int n = indexes.size();
+            for (int i = 0; i + 2 < idx.size(); i++) {
+                int a = idx[i];
+                int b = idx[i + 1];
+                int c = idx[i + 2];
 
-            for (int i = 0; i < n; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    for (int k = j + 1; k < n; k++) {
-
-                        int a = indexes[i];
-                        int b = indexes[j];
-                        int c = indexes[k];
-
-                        int dist = abs(a - b) + abs(b - c) + abs(c - a);
-                        mini = min(mini, dist);
-                    }
-                }
+                int dist = abs(a - b) + abs(b - c) + abs(c - a);
+                mini = min(mini, dist);
             }
         }
 
