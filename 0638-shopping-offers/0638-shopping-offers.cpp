22db95b77ca019b71885f694b981b68f50bb62b9
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-12-18 15:03:11 -0500

    Time: 27 ms (43.12%), Space: 24 MB (23.3%) - LeetHub

---
+++ 0638-shopping-offers/0638-shopping-offers.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    unordered_map<string,int>mpp;
+    int dfs(vector<int>& price, vector<vector<int>>& special, vector<int>& needs){
+        string key;
+        for(int x:needs){
+            key+=to_string(x)+",";
+        }
+        if(mpp.count(key)) return mpp[key];
+        int minCost=0;
+        for(int i=0;i<needs.size();i++){
+            minCost+=needs[i]*price[i];
+        }
+        for(auto it:special){
+            bool valid=true;
+            vector<int> newNeed=needs;
+            for(int i=0;i<needs.size();i++){
+                if(it[i]>needs[i]){
+                    valid=false;
+                    break;
+                }
+                newNeed[i]-=it[i];
+            }
+            if(valid){
+                minCost=min(minCost,it.back()+dfs(price,special,newNeed));
+            }
+        }
+        return mpp[key]=minCost;
+    }
+    int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
+        return dfs(price,special,needs);
+    }
+};
