39eccb4c01ac816deddd5bb70fc32e2af036d6dc
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-12-22 16:55:42 -0500

    Time: 0 ms (100%), Space: 10.5 MB (69.43%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -11,21 +11,11 @@
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
-        ListNode* temp=head;
-        vector<int> value;
-        while(temp!=NULL){
-            value.push_back(temp->val);
-            temp=temp->next;
-        }
-        reverse(value.begin(),value.end());
-        int power=0,ans=0;
-        for(auto it:value){
-            if(it==1){
-                ans+=1<<power;
-            }
-            power++;
+        int ans=0;
+        while(head!=NULL){
+            ans=2*ans+head->val;
+            head=head->next;
         }
         return ans;
-
     }
 };
