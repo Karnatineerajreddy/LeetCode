d3a94b0f567463b08f841e1fd82098aefd4393bb
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-12-23 20:17:07 -0500

    Time: 0 ms (100%), Space: 15.2 MB (50.41%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        int n=mat.size(),m=mat[0].size();
-        if(n*m!=r*c) return mat;
-        vector<vector<int>> ans(r,vector<int> (c));
+        int m=mat.size(),n=mat[0].size();
+        if(r*c!=n*m) return mat;
+        vector<vector<int>> ans(r,vector<int>(c));
         for(int i=0;i<r*c;i++){
-            ans[i/c][i%c]=mat[i/m][i%m];
+            ans[i/c][i%c]=mat[i/n][i%n];
         }
         return ans;
-
     }
 };
