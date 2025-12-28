5259915e7016e41959ebbf29c34a039f7159d15b
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-12-28 16:28:56 -0500

    Time: 31 ms (14.72%), Space: 61.4 MB (18.27%) - LeetHub

--- 2861-maximum-number-of-alloys/2861-maximum-number-of-alloys.cpp
+++ 2861-maximum-number-of-alloys/2861-maximum-number-of-alloys.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    bool check(int n, int k, int budget, vector<vector<int>>& composition, vector<int>& stock, vector<int>& cost,long long mid,vector<int>& comp){
-        long long totalCost=0;
+    bool check(int n,int k,int budget,vector<int>& comp,vector<int>& stock,vector<int>& cost,long long mid){
+        long long maxCost=0;
         for(int i=0;i<n;i++){
-            totalCost+=max(comp[i]*mid-stock[i],0ll)*cost[i];
+            maxCost+=max((mid*comp[i])-stock[i],0ll)*cost[i];
         }
-        return totalCost<=budget;
+        return maxCost<=budget;
     }
     int maxNumberOfAlloys(int n, int k, int budget, vector<vector<int>>& composition, vector<int>& stock, vector<int>& cost) {
         long long ans=0;
-        for(auto it : composition){
+        for(auto it:composition){
             long long i=0,j=1e12,res=0;
             while(i<=j){
                 int mid=(i+j)/2;
-                if(check(n,k,budget,composition,stock,cost,mid,it)){
+                if(check(n,k,budget,it,stock,cost,mid)){
                     res=mid;
                     i=mid+1;
                 }
