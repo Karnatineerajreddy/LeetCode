b3f932463895c8137541308aa55faea64f523aae
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-12-30 16:44:51 -0500

    Time: 0 ms (100%), Space: 10.9 MB (80.59%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int mctFromLeafValues(vector<int>& arr) {
-        int ans=0;
+        int minSum=0;
         stack<int> st;
         st.push(INT_MAX);
-        for(auto it : arr){
+        for(auto it:arr){
             while(st.top()<=it){
-                int mid=st.top();
+                int sum=st.top();
                 st.pop();
-                ans+=mid*min(st.top(),it);
+                minSum+=sum*min(st.top(),it);
             }
             st.push(it);
         }
         while(st.size()>2){
-            int mid=st.top();
+            int sum=st.top();
             st.pop();
-            ans+=mid*st.top();
+            minSum+=sum*st.top();
         }
-        return ans;
+        return minSum;
     }
 };
