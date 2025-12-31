bfd7c74587130eab5ca923f016492254396bc95e
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-12-31 13:57:34 -0500

    Time: 39 ms (75.22%), Space: 18.7 MB (72.4%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -14,27 +14,25 @@ public:
         }
         return first==s1.size() && second==s2.size();
     }
-    bool comp(string& s1,string& s2){
-        return s1.size() < s2.size();
-    }
-    int longestStrChain(vector<string>& arr) {
-        sort(arr.begin(), arr.end(),
-             [](const string& a, const string& b) {
-                 return a.size() < b.size();});
-        int n=arr.size();
-        vector<int> dp(n,1);
-        int maxi=1;
-        for(int i=0;i<n;i++){
-            for(int prev=0;prev<i;prev++){
-                if(check(arr[i],arr[prev]) && 1+dp[prev]>dp[i]){
-                    dp[i]=1+dp[prev];
-                }
-            }
-            if(dp[i]>maxi){
-                maxi=dp[i];
+    int memo(int i,vector<string>& arr,vector<int>& dp){
+        if(dp[i]!=-1) return dp[i];
+        int best=1;
+        for(int j=i+1;j<arr.size();j++){
+            if(check(arr[j],arr[i])){
+                best=max(best,1+memo(j,arr,dp));
             }
         }
+        return dp[i]=best;
+    }
+    int longestStrChain(vector<string>& arr) {
+        sort(arr.begin(), arr.end(),[](const string& a, const string& b) {
+            return a.size() < b.size();
+        });
+        int maxi=1;
+        vector<int> dp(arr.size(),-1);
+        for(int i=0;i<arr.size();i++){
+            maxi=max(maxi,memo(i,arr,dp));
+        }     
         return maxi;
-
     }
 };
