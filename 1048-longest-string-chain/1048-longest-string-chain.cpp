48350ed502ca88b746540b6adc6f22d2c2d73d49
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2025-12-31 13:06:29 -0500

    Time: 44 ms (59.8%), Space: 18.6 MB (94.96%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool check(string& s1,string& s2){
+        if(s1.size()!=s2.size()+1) return false;
+        int first=0,second=0;
+        while(first<s1.size()){
+            if(second < s2.size() && s1[first] == s2[second]){
+                first++;
+                second++;
+            }
+            else{
+                first++;
+            }
+        }
+        return first==s1.size() && second==s2.size();
+    }
+    bool comp(string& s1,string& s2){
+        return s1.size() < s2.size();
+    }
+    int longestStrChain(vector<string>& arr) {
+        sort(arr.begin(), arr.end(),
+             [](const string& a, const string& b) {
+                 return a.size() < b.size();});
+        int n=arr.size();
+        vector<int> dp(n,1);
+        int maxi=1;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(check(arr[i],arr[prev]) && 1+dp[prev]>dp[i]){
+                    dp[i]=1+dp[prev];
+                }
+            }
+            if(dp[i]>maxi){
+                maxi=dp[i];
+            }
+        }
+        return maxi;
+
+    }
+};
