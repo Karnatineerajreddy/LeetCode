91178285d760346e5fa5c96e4753a99bdcd5642a
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2026-01-05 17:15:06 -0500

    Time: 0 ms (100%), Space: 8.9 MB (91.29%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.cpp
+++ 0500-keyboard-row/0500-keyboard-row.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
-    bool checkFirstRow(string s){
-        string firstRow="qwertyuiop";
-        for(auto it:s){
-            it=tolower(it);
-            if(firstRow.find(it)==string :: npos){
-                return false;
-            }
-        }
-        return true;
-    }
-    bool checkSecondRow(string s){
-        string secondRow="asdfghjkl";
-        for(auto it:s){
-            it=tolower(it);
-            if(secondRow.find(it)==string :: npos){
-                return false;
-            }
-        }
-        return true;
-    }
-    bool checkThirdRow(string s){
-        string thirdRow="zxcvbnm";
-        for(auto it:s){
-            it=tolower(it);
-            if(thirdRow.find(it)==string :: npos){
-                return false;
-            }
-        }
-        return true;
-    }
     vector<string> findWords(vector<string>& words) {
+        string firstRow="qwertyuiop";
+        string secondRow="asdfghjkl";
+        string thirdRow="zxcvbnm";
         vector<string> ans;
         for(auto it:words){
-            if(checkFirstRow(it) || checkSecondRow(it) || checkThirdRow(it)){
-                ans.push_back(it);
+            string row;
+            char first=tolower(it[0]);
+            if(firstRow.find(first)!=string::npos) row=firstRow;
+            else if(secondRow.find(first)!=string::npos) row=secondRow;
+            else row=thirdRow;
+            bool valid=true;
+            for(auto word:it){
+                if(row.find(tolower(word))==string::npos){
+                    valid=false;
+                    break;
+                }
             }
+            if(valid) ans.push_back(it);
+
         }
         return ans;
     }
