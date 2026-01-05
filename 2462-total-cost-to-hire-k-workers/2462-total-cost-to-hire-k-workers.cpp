94a2efa7257d63be0f6d5d4e8a0d3720d57dc99b
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2026-01-05 16:55:03 -0500

    Time: 36 ms (76.9%), Space: 76.7 MB (75.26%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
-        long long totalCost=0;
         priority_queue<int,vector<int>,greater<int>> pq1,pq2;
         int count=0,i=0,j=costs.size()-1;
+        long long minSum=0;
         while(count<k){
             while(pq1.size()!=candidates && i<=j) pq1.push(costs[i++]);
             while(pq2.size()!=candidates && j>=i) pq2.push(costs[j--]);
-            int a=pq1.size() >0 ? pq1.top() : INT_MAX;
-            int b=pq2.size() > 0 ? pq2.top() :INT_MAX;
+            int a=pq1.size()>0 ? pq1.top() : INT_MAX;
+            int b=pq2.size()>0 ? pq2.top() : INT_MAX;
             if(a<=b){
-                totalCost+=a;
+                minSum+=a;
                 pq1.pop();
             }
             if(b<a){
-                totalCost+=b;
+                minSum+=b;
                 pq2.pop();
             }
             count++;
         }
-        return totalCost;
+        return minSum;
     }
 };
