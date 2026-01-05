fb67729fe4617574fc704e20b9d1eea49215f4e6
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2026-01-05 15:13:52 -0500

    Time: 30 ms (16.22%), Space: 61.4 MB (17.3%) - LeetHub

--- 2861-maximum-number-of-alloys/2861-maximum-number-of-alloys.cpp
+++ 2861-maximum-number-of-alloys/2861-maximum-number-of-alloys.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    bool check(int n,int k,int budget,vector<int>& comp,vector<int>& stock,vector<int>& cost,long long mid){
-        long long maxCost=0;
+    bool check(int n, int k, int budget, vector<int>& comp, vector<int>& stock, vector<int>& cost,long long mid){
+        long long maxBudget=0;
         for(int i=0;i<n;i++){
-            maxCost+=max((mid*comp[i])-stock[i],0ll)*cost[i];
+            maxBudget+=(max(mid*comp[i]-stock[i],0ll)*cost[i]);
         }
-        return maxCost<=budget;
+        return maxBudget<=budget;
     }
     int maxNumberOfAlloys(int n, int k, int budget, vector<vector<int>>& composition, vector<int>& stock, vector<int>& cost) {
-        long long ans=0;
-        for(auto it:composition){
-            long long i=0,j=1e12,res=0;
+        int ans=0;
+        for(auto comp:composition){
+            long long i=0,j=1e12,value=0;
             while(i<=j){
-                int mid=(i+j)/2;
-                if(check(n,k,budget,it,stock,cost,mid)){
-                    res=mid;
+                long long mid=(i+j)/2;
+                if(check(n,k,budget,comp,stock,cost,mid)){
+                    value=mid;
                     i=mid+1;
                 }
                 else{
                     j=mid-1;
                 }
             }
-            ans=max(ans,res);
+            ans=max((int)value,ans);
         }
         return ans;
     }
