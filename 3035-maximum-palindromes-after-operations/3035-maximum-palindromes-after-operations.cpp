05c7b3795a22f3231f055560bd917875bdbc42f3
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2026-01-06 13:35:23 -0500

    Time: 16 ms (27.01%), Space: 33.4 MB (14.22%) - LeetHub

--- 3035-maximum-palindromes-after-operations/3035-maximum-palindromes-after-operations.cpp
+++ 3035-maximum-palindromes-after-operations/3035-maximum-palindromes-after-operations.cpp
@@ -6,19 +6,19 @@ public:
             wordSize.push_back(it.size());
         }
         sort(wordSize.begin(),wordSize.end());
-        unordered_map<char,int> freq;
+        unordered_map<char,int> wordFreq;
         for(auto it:words){
             for(auto c:it){
-                freq[c]++;
+                wordFreq[c]++;
             }
         }
-        int even=0,odd=0,n=wordSize.size();
-        for(auto it:freq){
+        int even=0,odd=0;
+        for(auto it:wordFreq){
             even+=it.second/2;
             odd+=it.second%2;
         }
         int ans=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<wordSize.size();i++){
             if(wordSize[i]%2==1){
                 if(odd){
                     odd--;
