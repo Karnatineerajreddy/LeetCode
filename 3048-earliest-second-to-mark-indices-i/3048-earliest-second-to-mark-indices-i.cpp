afcb9f5f6a431eadee073a4b00d7decbaa10cf14
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2026-01-06 15:55:31 -0500

    Time: 8 ms (61.63%), Space: 43.2 MB (58.14%) - LeetHub

--- 3048-earliest-second-to-mark-indices-i/3048-earliest-second-to-mark-indices-i.cpp
+++ 3048-earliest-second-to-mark-indices-i/3048-earliest-second-to-mark-indices-i.cpp
@@ -1,48 +1,40 @@
 class Solution {
 public:
-    bool checkPossible(vector<int>& nums, vector<int>& checkToZero, int s) {
-        int n = nums.size();
-        vector<int> last(n, -1);
-
-        for (int i = 0; i <= s; i++) {
-            last[checkToZero[i]] = i;
+    bool checkPossible(vector<int>& nums, vector<int>& checkTwoZero,int s){
+        vector<int> last(nums.size(),-1);
+        for(int i=0;i<=s;i++){
+            last[checkTwoZero[i]]=i;
         }
-        for (int i = 0; i < n; i++) {
-            if (last[i] == -1) return false;
+        for(int i=0;i<nums.size();i++){
+            if(last[i]==-1) return false;
         }
-        int power = 0;
-        for (int i = 0; i <= s; i++) {
-            int idx = checkToZero[i];
-
-            if (i == last[idx]) {
-                if (nums[idx] > power) return false;
-                power -= nums[idx];
-            } else {
-                power++;
+        int power=0;
+        for(int i=0;i<=s;i++){
+            int idx=checkTwoZero[i];
+            if(i==last[idx]){
+                if(power<nums[idx]) return false;
+                power-=nums[idx];
             }
+            else power++;
         }
         return true;
     }
-
     int earliestSecondToMarkIndices(vector<int>& nums, vector<int>& changeIndices) {
-        int m = changeIndices.size();
-        vector<int> changeToZero = changeIndices;
-
-        for (int& x : changeToZero) x--; // make 0-based
-
-        int i = 0, j = m - 1, ans = -1;
-
-        while (i <= j) {
-            int mid = (i + j) / 2;
-
-            if (checkPossible(nums, changeToZero, mid)) {
-                ans = mid;
-                j = mid - 1;   // try earlier
-            } else {
-                i = mid + 1;   // try later
+        vector<int> checkTwoZero=changeIndices;
+        for(auto& it:checkTwoZero){
+            it--;
+        }
+        int i=0,j=checkTwoZero.size()-1,ans=-1;
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(checkPossible(nums,checkTwoZero,mid)){
+                ans=mid;
+                j=mid-1;
+            }
+            else{
+                i=mid+1;
             }
         }
-
-        return ans == -1 ? -1 : ans + 1;
+        return ans==-1? -1 : ans+1;
     }
 };
