97bb11d01a93e9886afa80fdbde5b3e82c85f0f7
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2026-01-07 11:19:30 -0500

    Time: 23 ms (46.15%), Space: 23.9 MB (22.11%) - LeetHub

--- 0638-shopping-offers/0638-shopping-offers.cpp
+++ 0638-shopping-offers/0638-shopping-offers.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     unordered_map<string,int> mpp;
-    int findMinPrice(vector<int>& price,vector<vector<int>>& special,vector<int>& needs){
-        string key="";
+    int findPrice(vector<int>& price, vector<vector<int>>& special, vector<int>& needs){
+        string key;
         for(auto it:needs){
-            key+=to_string(it) + ',';
+            key+=to_string(it) + ",";
         }
         if(mpp.count(key)) return mpp[key];
         int minPrice=0;
-        for(int i=0;i<price.size();i++){
-            minPrice+=(price[i]*needs[i]);
+        for(int i=0;i<needs.size();i++){
+            minPrice+=(needs[i]*price[i]);
         }
-        for(auto& it:special){
+        for(auto it:special){
             bool valid=true;
             vector<int> newNeeds=needs;
             for(int i=0;i<needs.size();i++){
@@ -22,12 +22,12 @@ public:
                 newNeeds[i]-=it[i];
             }
             if(valid){
-                minPrice=min(minPrice,it.back()+findMinPrice(price,special,newNeeds));
+                minPrice=min(minPrice,it.back()+findPrice(price,special,newNeeds));
             }
         }
         return mpp[key]=minPrice;
     }
     int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
-        return findMinPrice(price,special,needs);
+        return findPrice(price,special,needs);
     }
 };
