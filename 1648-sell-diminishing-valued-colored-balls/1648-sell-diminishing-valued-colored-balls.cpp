2bc449bbb4a13a8b556390add84abecb55940b8b
Neeraj Reddy Karnati <101508032+Karnatineerajreddy@users.noreply.github.com>
2026-01-06 20:10:00 -0500

    Time: 27 ms (73.21%), Space: 68.8 MB (66.79%) - LeetHub

--- 1648-sell-diminishing-valued-colored-balls/1648-sell-diminishing-valued-colored-balls.cpp
+++ 1648-sell-diminishing-valued-colored-balls/1648-sell-diminishing-valued-colored-balls.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    bool check(vector<int>& inventory, int orders,int mid){
-        int maxCount=0;
-        for(int i=0;i<inventory.size();i++){
-            if(inventory[i]>=mid){
-                maxCount+=(inventory[i]-mid+1);
+    bool findLastValue(vector<int>& inventory,int orders,int mid){
+        int ballSum=0;
+        for(auto it:inventory){
+            if(it>=mid){
+                ballSum+=(it-mid+1);
             }
-            if(maxCount>=orders) return true;
+            if(ballSum>=orders) return true;
         }
         return false;
     }
     int maxProfit(vector<int>& inventory, int orders) {
-        int mod=1e9+7,minValue=1;
+        int mod=1e9+7;
         sort(inventory.begin(),inventory.end(),greater<int>());
-        int left=1,right=inventory[0];
-        while(left<=right){
-            int mid=(left+right)/2;
-            if(check(inventory,orders,mid)){
+        int i=1,j=inventory[0],minValue=1;
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(findLastValue(inventory,orders,mid)){
                 minValue=mid;
-                left=mid+1;
+                i=mid+1;
             }
             else{
-                right=mid-1;
+                j=mid-1;
             }
         }
         long long profit=0,sold=0;
-        for(auto it:inventory){
-            if(it>minValue){
-                long long  count=(it-minValue);
+        for(int i=0;i<inventory.size();i++){
+            if(inventory[i]>minValue){
+                long long count=inventory[i]-minValue;
                 sold+=count;
-                long long sum=(long long)(it+minValue+1) * count/2;
+                long long sum=(long long)(inventory[i]+minValue+1)*count/2;
                 profit=(profit+sum)%mod;
             }
         }
         profit=(profit+(orders-sold)*minValue)%mod;
         return profit;
+
     }
 };
